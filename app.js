5b218499ef00eb69959d59f9085c417b621c2036
Ranajoy Ghosh <ranajoy.ghosh@gmail.com>
2021-02-22 00:49:50 +0100

    integrating the part for SendGrid

--- app.js
+++ app.js
@@ -6,26 +6,46 @@ const app = express();
 require('dotenv/config');
 const usersRoutes = require("./routes/users");
 const port = process.env.PORT;
+const sgMail = require('@sendgrid/mail');
+const SG_API_KEY = process.env.SENDGRID_API_KEY;
+const User = require('./models/User');
 
 app.use(cors());
 app.use(bodyParser.json());
-
-
 app.use('/users', usersRoutes);
 
-
 mongoose.connect(process.env.DB_CONNECTION,
-    { useNewUrlParser: true},
-    ()=> {
-    console.log('connected to db');
-    }
+    { useNewUrlParser : true },
+    ()=>{
+        console.log('connected to db');
+    },
 );
 
-// Send message for default URL
-app.get('/', (req, res) => res.send('Welcome to the RedditLetter API'));
+let allUsers = [];
 
-// Launch app to listen to specified port
-app.listen(port, function () {
+(async function(){
+    allUsers = await User.find();
+    console.log(allUsers);
+})();
+
+app.listen(port, function(){
     console.log("Running RedditLetter API on port " + port);
+
 });
+
+sgMail.setApiKey(SG_API_KEY);
+const mailBody = {
+    from : 'ranajoy121@gmail.com',
+    to : allUsers.map(u=>u.email),
+    subject : 'Daily Reddit Digest',
+    html : '',
+};
+
+//SENDING OUT AN EMAIL TO EACH USER
+sgMail.send(mailBody).then(res=>console.log('Email sent', res)).catch(err=>console.log(err.message));
+
+
+
+
+
 
